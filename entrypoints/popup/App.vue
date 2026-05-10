c2bca05f7b64b44967d6dca193adc6805be80723
Darren <iammdchung@gmail.com>
2026-05-10 16:12:59 +0800

    chore: change icon

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -262,7 +262,7 @@ async function handleFileChange(event: Event) {
       @change="handleFileChange"
     />
     <header v-if="!detailCategory" class="flex items-center gap-2 mb-4">
-      <img class="w-8 h-8" src="/icon/128.png" alt="logo" />
+      <img class="w-10 h-10" src="/icon/128.png" alt="logo" />
       <div class="flex-1 min-w-0">
         <h1 class="text-base font-semibold leading-tight truncate">
           Search Image Blocker
